4001f0137725f204c543da606058364385c82f2a
Pravesh Narayan <artbypravesh15@gmail.com>
2019-05-16 23:18:52 +1000

    Minor syntax errors fixed with phpcbf

--- src/template-parts/content-page.php
+++ src/template-parts/content-page.php
@@ -20,10 +20,12 @@
 		<?php
 		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'abp' ),
-			'after'  => '</div>',
-		) );
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'abp' ),
+				'after'  => '</div>',
+			)
+		);
 		?>
 	</div><!-- .entry-content -->
 
